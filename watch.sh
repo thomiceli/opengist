ad986572264259b0fbfcc48907551a399eebfc37
HeCorr <hccaseclone@gmail.com>
2023-05-06 23:05:25 -0300

    modify watch.sh to be compatible with Task
    it accepts an optional `task` argument which makes it run task commands instead of make commands (the default if no argument is supplied).
    
    since I'm on Windows I had to add a parameter to .gitattributes so I could save watch.sh with LF line endings, otherwise it would fail to be executed in Linux.

--- watch.sh
+++ watch.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 set -euo pipefail
 
-make watch_frontend &
-make watch_backend &
+if [ "$1" == "task" ]; then
+    task watch_frontend &
+    task watch_backend &
+else
+    make watch_frontend &
+    make watch_backend &
+fi
 
 trap 'kill $(jobs -p)' EXIT
 wait

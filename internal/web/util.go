fced14ac6cb4fff9c0f3e11a3420bd3dda6f2b2a
Thomas Miceli <27960254+thomiceli@users.noreply.github.com>
2023-12-16 01:27:00 +0100

    Add healthcheck endpoint (#170)

--- internal/web/util.go
+++ internal/web/util.go
@@ -168,7 +168,7 @@ func validateReservedKeywords(fl validator.FieldLevel) bool {
 	name := fl.Field().String()
 
 	restrictedNames := map[string]struct{}{}
-	for _, restrictedName := range []string{"assets", "register", "login", "logout", "settings", "admin-panel", "all", "search", "init"} {
+	for _, restrictedName := range []string{"assets", "register", "login", "logout", "settings", "admin-panel", "all", "search", "init", "healthcheck"} {
 		restrictedNames[restrictedName] = struct{}{}
 	}
 

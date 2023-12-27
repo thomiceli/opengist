4bba26daf6c5f19d65de233dfe30d2c91d23db23
Jacob Hands <jacob@gogit.io>
2023-12-27 10:24:52 -0600

    Add log output config option (#172)
    Co-authored-by: Thomas Miceli <27960254+thomiceli@users.noreply.github.com>

--- internal/utils/slice.go
+++ internal/utils/slice.go
@@ -8,3 +8,15 @@ func SliceContains(slice []string, item string) bool {
 	}
 	return false
 }
+
+func RemoveDuplicates[T string | int](sliceList []T) []T {
+	allKeys := make(map[T]bool)
+	list := []T{}
+	for _, item := range sliceList {
+		if _, value := allKeys[item]; !value {
+			allKeys[item] = true
+			list = append(list, item)
+		}
+	}
+	return list
+}

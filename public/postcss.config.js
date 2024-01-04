d85917bfb2a64a3387900f260ced68de30cf9e2b
Thomas Miceli <27960254+thomiceli@users.noreply.github.com>
2024-01-04 04:44:56 +0100

    Small fixes (#196)

--- public/postcss.config.js
+++ public/postcss.config.js
@@ -2,7 +2,9 @@ module.exports = {
   plugins: {
     'postcss-import': {},
     'tailwindcss/nesting': {},
-    tailwindcss: {},
+    tailwindcss: {
+      config: "./public/tailwind.config.js",
+    },
     autoprefixer: {},
     'postcss-selector-namespace': {namespace() {return (process.env.EMBED) ? '.opengist-embed' : '';}},
     cssnano: {},

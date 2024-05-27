1ceb2a0813845528391c03d2267bc091b414508e
Thomas Miceli <tho.miceli@gmail.com>
2024-05-27 23:51:04 +0200

    Fix CI check for additional translations only

--- scripts/check-translations.sh
+++ scripts/check-translations.sh
@@ -8,8 +8,6 @@ sed -i '/^\s*$/d' sorted_reference_keys.txt
 
 for new_file in internal/i18n/locales/*.yml; do
     filename=$(basename $new_file)
-    echo ""
-    echo "Checking $filename..."
 
     # Extract keys from the current file and sort them
     sort <(awk -F':' '{print $1}' $new_file) > sorted_new_keys.txt
@@ -18,17 +16,12 @@ for new_file in internal/i18n/locales/*.yml; do
     comm -3 sorted_reference_keys.txt sorted_new_keys.txt > differences.txt
 
     if [ -s differences.txt ]; then
-        echo "Error in $filename: The YAML file has differences in keys."
         while IFS= read -r line; do
             if [[ $line == $'\t'* ]]; then
                 echo "+ Additional key in $filename: $(echo $line | awk '{$1=$1; print}')"
-            else
-                echo "- Missing key in $filename: $(echo $line | awk '{$1=$1; print}')"
+                differences_found=1
             fi
         done < differences.txt
-        differences_found=1
-    else
-        echo "All keys in $filename match perfectly."
     fi
 
     rm sorted_new_keys.txt

01f7821fa494b1d150e9f701bf84456bff072668
phanirithvij <phanirithvij2000@gmail.com>
2024-11-26 18:15:42 +0530

    init git config failure -> warn
    Signed-off-by: phanirithvij <phanirithvij2000@gmail.com>

--- internal/cli/main.go
+++ internal/cli/main.go
@@ -92,12 +92,12 @@ func Initialize(ctx *cli.Context) {
 			"Current git version: " + gitVersion)
 	}
 
-	if err := git.InitGitConfig(); err != nil {
-		log.Fatal().Err(err).Send()
-	}
-
 	homePath := config.GetHomeDir()
 	log.Info().Msg("Data directory: " + homePath)
+
+	if err := git.InitGitConfig(); err != nil {
+		log.Warn().Msg("Git config modification failed, ensure " + homePath + " is added to git safe directories, and receive.advertisePushOptions is set to true.")
+	}
 
 	if err := createSymlink(homePath, ctx.String("config")); err != nil {
 		log.Fatal().Err(err).Msg("Failed to create symlinks")

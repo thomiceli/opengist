0e903c91f4599d74cde7342197e7e6d437a0ef3a
Thomas Miceli <tho.miceli@gmail.com>
2025-01-26 23:55:49 +0100

    Use npm package

--- public/main.ts
+++ public/main.ts
@@ -12,10 +12,13 @@ import 'dayjs/locale/pt';
 import 'dayjs/locale/ru';
 import 'dayjs/locale/zh';
 import localizedFormat from 'dayjs/plugin/localizedFormat';
+import jdenticon from 'jdenticon/standalone';
 
 dayjs.extend(relativeTime);
 dayjs.extend(localizedFormat);
 dayjs.locale(window.opengist_locale || 'en');
+
+jdenticon.update("[data-jdenticon-value]")
 
 document.addEventListener('DOMContentLoaded', () => {
     const themeMenu = document.getElementById('theme-menu')!;

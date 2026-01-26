00367e50722f6a9cdd5e24ada2d0b71ca43e3d57
Thomas Miceli <tho.miceli@gmail.com>
2026-01-26 18:24:35 +0700

    Add access tokens

---
+++ internal/web/handlers/settings/access_token.go
@@ -0,0 +1,75 @@
+package settings
+
+import (
+	"strconv"
+
+	"github.com/thomiceli/opengist/internal/db"
+	"github.com/thomiceli/opengist/internal/i18n"
+	"github.com/thomiceli/opengist/internal/validator"
+	"github.com/thomiceli/opengist/internal/web/context"
+)
+
+func AccessTokens(ctx *context.Context) error {
+	user := ctx.User
+
+	tokens, err := db.GetAccessTokensByUserID(user.ID)
+	if err != nil {
+		return ctx.ErrorRes(500, "Cannot get access tokens", err)
+	}
+
+	ctx.SetData("accessTokens", tokens)
+	ctx.SetData("settingsHeaderPage", "tokens")
+	ctx.SetData("htmlTitle", ctx.TrH("settings"))
+	return ctx.Html("settings_tokens.html")
+}
+
+func AccessTokensProcess(ctx *context.Context) error {
+	user := ctx.User
+
+	dto := new(db.AccessTokenDTO)
+	if err := ctx.Bind(dto); err != nil {
+		return ctx.ErrorRes(400, ctx.Tr("error.cannot-bind-data"), err)
+	}
+
+	if err := ctx.Validate(dto); err != nil {
+		ctx.AddFlash(validator.ValidationMessages(&err, ctx.GetData("locale").(*i18n.Locale)), "error")
+		return ctx.RedirectTo("/settings/tokens")
+	}
+
+	token := dto.ToAccessToken()
+	token.UserID = user.ID
+
+	plainToken, err := token.GenerateToken()
+	if err != nil {
+		return ctx.ErrorRes(500, "Cannot generate token", err)
+	}
+
+	if err := token.Create(); err != nil {
+		return ctx.ErrorRes(500, "Cannot create access token", err)
+	}
+
+	// Show the token once to the user
+	ctx.AddFlash(ctx.Tr("settings.token-created"), "success")
+	ctx.AddFlash(plainToken, "success")
+	return ctx.RedirectTo("/settings/tokens")
+}
+
+func AccessTokensDelete(ctx *context.Context) error {
+	user := ctx.User
+	tokenID, err := strconv.Atoi(ctx.Param("id"))
+	if err != nil {
+		return ctx.RedirectTo("/settings/tokens")
+	}
+
+	token, err := db.GetAccessTokenByID(uint(tokenID))
+	if err != nil || token.UserID != user.ID {
+		return ctx.RedirectTo("/settings/tokens")
+	}
+
+	if err := token.Delete(); err != nil {
+		return ctx.ErrorRes(500, "Cannot delete access token", err)
+	}
+
+	ctx.AddFlash(ctx.Tr("settings.token-deleted"), "success")
+	return ctx.RedirectTo("/settings/tokens")
+}
